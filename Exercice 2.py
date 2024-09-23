24c5b612591a1fd8652cf14d8218d27e764a281b
Soufiane Mellouki Filali <soufiane.melloukifilali@student.hepl.be>
2024-09-23 09:21:47 +0200

    Page 63

--- Exercice 2.py
+++ Exercice 2.py
@@ -1,4 +1,16 @@
-from machine import ADC
-from utime import sleep
+from machine import ADC,Pin,PWM
+import utime
 
+LED_PWM = PWM(Pin(18))
 ROTARY_ANGLE_SENSOR = ADC(0)
+LED_PWM.freq(500)
+val = 0
+
+while val<65535:
+    val = val+50
+    utime.sleep_ms(1)
+    LED_PWM.duty_u16(val)
+while val>0:
+    val = val -50
+    utime.sleep_ms(1)
+    LED_PWM.duty_u16(val)
