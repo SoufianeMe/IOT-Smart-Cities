008f39e99149eeefd334661fdfcaea0f002308b1
Soufiane Mellouki Filali <soufiane.melloukifilali@student.hepl.be>
2024-10-01 19:46:45 +0200

    Finition de l'exercice 3

--- Exercice 3.py
+++ Exercice 3.py
@@ -10,12 +10,9 @@ d = LCD1602(i2c, 2, 16)
 d.display()
 
 dht = DHT(18)
-
 buzzer = PWM(Pin(16))
-
 LED = PWM(Pin(20))
 LED.freq(500)
-
 ROTARY_ANGLE_SENSOR = ADC(0)
 
 start = time.ticks_ms()
@@ -24,61 +21,53 @@ startlcd = time.ticks_ms()
 startcli = time.ticks_ms()
 
 val = 0
+phase = "Montante"
+
 def Breathing(vitesse):
     global val,startled,phase
     if time.ticks_diff(time.ticks_ms(),startled) > 10:
         startled = time.ticks_ms()
-        if val == 0:
-            val = val + vitesse
+        if val <= 0:
             phase = "Montante"
         if val >= 65000:
-            val = val - vitesse
             phase = "Descendante"
         if phase == "Montante":
-            val = val + vitesse
-            LED.duty_u16(val)
+            val += vitesse
         if phase == "Descendante":
-            val = val - vitesse
-            LED.duty_u16(val)
+            val -= vitesse
+        LED.duty_u16(val)
         
 def PrintLed(text1,text2):
-    d.clear()
     d.setCursor(0,0)
     d.print(text1)
     d.setCursor(0,1)
     d.print(text2)
     
-def Clignoter():
+def Clignoter(tempset):
     global startcli
-    if time.ticks_diff(time.ticks_ms(),startcli) > 500 and time.ticks_diff(time.ticks_ms(),startcli) < 600:
-        PrintLed("ALARM","")
+    elapsed = time.ticks_diff(time.ticks_ms(),startcli)
+    if 300 < elapsed < 900:
+        PrintLed("ALARM",f"Tempset: {tempset:.2f}")
     if time.ticks_diff(time.ticks_ms(),startcli) > 1000:
         d.clear()
         startcli=time.ticks_ms()
-    #if time.ticks_diff(time.ticks_ms(),startcli) > 300:
-    #    PrintLed("ALARM","")
-    
-def Defiler():
-    global startcli
-    list=["ALARM"," ALARM","  ALARM","   ALARM","    ALARM","     ALARM","      ALARM","       ALARM"]
-    if time.ticks_diff(time.ticks_ms(),startcli) > 500 and time.ticks_diff(time.ticks_ms(),startcli) < 600:
-        PrintLed(list[0],"")
-    if time.ticks_diff(time.ticks_ms(),startcli) > 800 and time.ticks_diff(time.ticks_ms(),startcli) < 1000:
-        PrintLed(list[1],"")
-    if time.ticks_diff(time.ticks_ms(),startcli) > 1200 and time.ticks_diff(time.ticks_ms(),startcli) < 1400:
-        PrintLed(list[2],"")
-    if time.ticks_diff(time.ticks_ms(),startcli) > 1600 and time.ticks_diff(time.ticks_ms(),startcli) < 1800:
-        PrintLed(list[3],"")
-    if time.ticks_diff(time.ticks_ms(),startcli) > 2000 and time.ticks_diff(time.ticks_ms(),startcli) < 2200:
-        PrintLed(list[4],"")
-        startcli=time.ticks_ms()
 
+def Defiler(tempset):
+    global startcli
+    elapsed = time.ticks_diff(time.ticks_ms(),startcli)
+    list=["ALARM"," ALARM","  ALARM","   ALARM","    ALARM","     ALARM","      ALARM","       ALARM"]
+    index = (elapsed // 300) % len(list)
+    if index < len(list):
+        PrintLed(list[index],f"Tempset: {tempset:.2f}")
+    if elapsed > 2400:
+        d.clear()
+        startcli = time.ticks_ms()
         
     
 def Afficher(tempset,temp):
     global startlcd
     if time.ticks_diff(time.ticks_ms(),startlcd) > 100:
-        PrintLed("Set: "+str(tempset),"Ambient: "+str(temp))
+        PrintLed(f"Set: {tempset:.2f}",f"Ambient: {temp:.2f}")
         startlcd = time.ticks_ms()
     
 temp,humid = dht.readTempHumid()
@@ -89,17 +78,17 @@ while True:
     if time.ticks_diff(time.ticks_ms(),start) > 1000:
         start = time.ticks_ms()
         temp,humid = dht.readTempHumid()
-        tempset = (ROTARY_ANGLE_SENSOR.read_u16()*35/65535) 
+        tempset = 15 + (ROTARY_ANGLE_SENSOR.read_u16()*20/65535)
     if temp < (tempset+3) and temp > tempset:
         Afficher(tempset,temp)
         buzzer.duty_u16(0)
         Breathing(500)
     elif temp > (tempset+3):
         buzzer.freq(500)
-        buzzer.duty_u16(1000)
+        buzzer.duty_u16(500)
         Breathing(2000)
-        #Clignoter()
-        Defiler()
+        #Clignoter(tempset)
+        Defiler(tempset)
     else:
         Afficher(tempset,temp)
         buzzer.duty_u16(0)
